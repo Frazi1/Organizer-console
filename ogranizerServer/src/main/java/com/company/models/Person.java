968db3c2fe121aa1cdd642f0cae215f99eb6a036
Fraz <diman896@gmail.com>
2017-10-23 22:10:06 +0300

    Person model fix.

--- ogranizerServer/src/main/java/com/company/models/Person.java
+++ ogranizerServer/src/main/java/com/company/models/Person.java
@@ -1,8 +1,16 @@
 package com.company.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
     private String name;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     public Person() {
